e44cb3d654132ff2077d7a0f8d2cf4cef67b03b3
faizasidd <faiza.siddiqi1@gmail.com>
2022-06-20 14:46:33 -0400

    GET /videos that responds with an array of videos

--- routes/videos.js
+++ routes/videos.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+
+const videosJsonPath = '../data/videos.json';
+const videos = require(videosJsonPath);
+
+router.get('/videos', (req, res) => {
+    return res.json(
+      videos.reduce((accumulator, curr) => {
+        accumulator.push({ // Reduce video info array down to the required attributes per video
+          id: curr.id,
+          title: curr.title,
+          channel: curr.channel,
+          image: curr.image,
+        });
+      
+        return accumulator;
+      }, [])
+    );
+  });
 
 
 
